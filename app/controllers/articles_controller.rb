b1170ac0f80fddd792b88eec2267c18f021aaea3
Amber <amber@amberwilkie.com>
2016-12-03 00:08:02 +0100

    Add article creation view and controller actions

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,20 @@
+class ArticlesController < ApplicationController
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = 'Item saved.'
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body, :link)
+  end
+end
