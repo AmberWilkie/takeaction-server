1369769f42417ba8ea777f6b5f51f0896fd68b30
Amber <amber@amberwilkie.com>
2016-12-03 21:03:07 +0100

    Better seeds for inequality articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.delete_all
 Cause.delete_all
 Action.delete_all
-Article.delete_all
 
 
-cause1 = Cause.create(name: 'Poverty')
-cause2 = Cause.create(name: 'Global Warming')
-cause3 = Cause.create(name: 'AIDS')
+
+cause1 = Cause.create(name: 'Inequality')
+cause2 = Cause.create(name: 'Food and Land Rights')
+cause3 = Cause.create(name: "Women's Rights")
+cause4 = Cause.create(name: 'Democratic Governance')
+cause5 = Cause.create(name: 'Education')
+cause6 = Cause.create(name: 'Emergencies and Conflict')
+cause7 = Cause.create(name: 'Climate Change')
+cause8 = Cause.create(name: 'HIV and AIDS')
+cause9 = Cause.create(name: 'Youth')
+
 
 action1 = Action.create(name: 'Tweet')
 action2 = Action.create(name: 'Volunteer')
@@ -22,6 +30,13 @@ action5 = Action.create(name: 'Email your legislator')
 action6 = Action.create(name: 'Join this Event')
 action7 = Action.create(name: 'Donate')
 
-article1 = Article.create(title: 'Climate Change is going to kill us all.', body: 'No, really.', link: 'http://www.climate.org', cause: cause2)
+article1 = Article.create(title: 'Some people have way more money than other people', body: 'No, really.', link: 'http://www.equality.org', cause: cause1)
 article1.action_items.create(action_id: action1.id)
 article1.action_items.create(action_id: action5.id)
+
+article2 = Article.create(title: "It's pretty seriously unfair how rich some people are", body: "It's cool when they work hard and give back and everything, obviously. But plenty of people are getting rich by <i>making</i> other people poor and that really sucks.", link: 'http://www.killtherich.com', cause: cause1)
+article2.action_items.create(action_id: action1.id)
+
+article3 = Article.create(title: 'Demonstration against Inequality!', body: 'Gather at Ideon', link: 'http://www.equality.org', location: 'Ideon Headquarters, Lund, Sweden', date: '12/12/16', cause: cause1)
+article3.action_items.create(action_id: action2.id)
+article3.action_items.create(action_id: action4.id)
