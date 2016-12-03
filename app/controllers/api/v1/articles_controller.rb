04cc3faf197f978be54afaff64cb638fb85b23dc
Amber <amber@amberwilkie.com>
2016-12-03 16:41:19 +0100

    Handle no params for cause

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,9 +1,13 @@
 class Api::V1::ArticlesController < ActionController::Base
 
   def index
-    cause = Cause.find_by(name: params[:cause])
-    @items = Article.where(cause: cause)
-    # Here we would put in all kinds of other search things so we could winnow down by peoples' interests.
+    unless params[:cause].nil?
+      cause = Cause.find_by(name: params[:cause].capitalize)
+      @items = Article.where(cause: cause)
+    else
+      @items = Article.all.limit(20)
+    end
+    # Here we would put in all kinds of other search things so we could winnow down by peoples' interests. The search information should be available to us in params (and created by the front-end client in their request.)
     render json: @items
   end
 end
