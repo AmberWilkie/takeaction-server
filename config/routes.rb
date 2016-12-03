d3baef0468fc5bb8ae5d3c72b09f1c8938c2fd71
Amber <amber@amberwilkie.com>
2016-12-03 11:54:00 +0100

    Add basic api for articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   resource :articles, controller: :articles, only: [:new, :create]
 
   root to: 'articles#new'
+
+  namespace :api do
+    namespace :v1 do
+      resources :articles, only: [:index], constraints: { format: /(json)/ }
+    end
+  end
 end
